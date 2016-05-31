292778b765e964692c26bc63b9315a5ead5a37b2
Jakub Scholz <jakub@scholz.cz>
2016-05-31 16:08:58 +0200

    Add Javadoc

--- src/main/java/cz/scholz/aliaskeymanager/AliasKeyManager.java
+++ src/main/java/cz/scholz/aliaskeymanager/AliasKeyManager.java
@@ -8,12 +8,19 @@ import java.security.PrivateKey;
 import java.security.cert.X509Certificate;
 
 /**
- * Created by schojak on 30.5.16.
+ * AliasKeyManager extends the X509ExtendedKeyManager with a wrapper which allows to always select predefined client certificate.
  */
 public class AliasKeyManager extends X509ExtendedKeyManager {
     X509ExtendedKeyManager original;
     String alias;
 
+    /**
+     * Creates an instance of AliasKeyManager based on instance of X509EntendedKeyManager.
+     *
+     * @param alias The key alias which should be used for cleint authentication
+     * @param original The original KeyManager which should be wrapped. All functionality unrelated to the client key
+     *                 selection will be taked from this KeyManager.
+     */
     public AliasKeyManager(String alias, X509ExtendedKeyManager original)
     {
         if (alias == null) {
@@ -28,22 +35,58 @@ public class AliasKeyManager extends X509ExtendedKeyManager {
         this.original = original;
     }
 
+    /**
+     * Returns always the selected key alias.
+     *
+     * @param s
+     * @param principals
+     * @return Key alias as a array
+     */
     public String[] getClientAliases(String s, Principal[] principals) {
         return new String[] { alias };
     }
 
+    /**
+     * Returns always the selected key alias.
+     *
+     * @param s
+     * @param principals
+     * @param socket
+     * @return Key alias as a String
+     */
     public String chooseClientAlias(String[] strings, Principal[] principals, Socket socket) {
         return alias;
     }
 
+    /**
+     * Falls back to the original KeyManager.
+     *
+     * @param s
+     * @param principals
+     * @return
+     */
     public String[] getServerAliases(String s, Principal[] principals) {
         return original.getServerAliases(s, principals);
     }
 
+    /**
+     * Falls back to the original KeyManager.
+     *
+     * @param s
+     * @param principals
+     * @param socket
+     * @return
+     */
     public String chooseServerAlias(String s, Principal[] principals, Socket socket) {
         return original.chooseServerAlias(s, principals, socket);
     }
 
+    /**
+     * Falls back to the original KeyManager.
+     *
+     * @param s
+     * @return
+     */
     public X509Certificate[] getCertificateChain(String s) {
         return original.getCertificateChain(s);
     }
@@ -52,10 +95,26 @@ public class AliasKeyManager extends X509ExtendedKeyManager {
         return original.getPrivateKey(s);
     }
 
+    /**
+     * Returns always the selected key alias.
+     *
+     * @param s
+     * @param principals
+     * @param socket
+     * @return Key alias as a String
+     */
     public String chooseEngineClientAlias(String[] keyType, Principal[] issuers, SSLEngine engine) {
         return alias;
     }
 
+    /**
+     * Falls back to the original KeyManager.
+     *
+     * @param keyType
+     * @param issuers
+     * @param engine
+     * @return
+     */
     public String chooseEngineServerAlias(String keyType, Principal[] issuers, SSLEngine engine) {
         return original.chooseEngineServerAlias(keyType, issuers, engine);
     }
